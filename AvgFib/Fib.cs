23cca602bc5507469a3b8ea452a6f7b230ee62b5
John Atkins <jdatkins@tql.com>
2021-05-25 17:22:02 -0400

    Add project files.

---
+++ AvgFib/Fib.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvgFib
+{
+    class Fib
+    {
+        public static List<int> GenerateSequence(int last = 200)
+        {
+            var fibs = new List<int>();
+            var a = 0;
+            fibs.Add(a);
+            var b = 1;
+            fibs.Add(b);
+            int c = 0;
+            while(c < last)
+            {
+                c = a + b;
+                fibs.Add(c);
+                a = b;
+                b = c;
+            }
+            return fibs;
+        }
+    }
+}
---
+++ AvgFib/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AvgFib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var next = 0.0m;
+            var avg = 0.0m;
+            var count = 0.0m;
+            var fib = new List<decimal>();
+            fib.Add(0);
+            fib.Add(1);
+            for(var i = 2; next < 200; i++)
+            {
+                next = fib[(i - 1)] + fib[(i - 2)];
+                fib.Add(next);
+            }
+            foreach(int nbr in fib)
+            {
+                avg += nbr;
+                count++;
+            }
+            avg = fib.Average();
+            Console.WriteLine($"Average of sequence: {avg}");
+        }
+    }
+}
